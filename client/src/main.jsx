c5ec2953baa1c5329d725215587fb2914dbb30c8
Teodor <elrynsier@gmail.com>
2025-03-13 13:05:01 +0200

    install react router, and set it in main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
