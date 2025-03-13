f457786ebb56efe8614384c4c349dc9c696fc954
Teodor <elrynsier@gmail.com>
2025-03-13 14:11:20 +0200

    improved navBar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,35 +3,24 @@ import { Link } from "react-router"
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container d-flex justify-content-between align-items-center py-2">
         <Link className="navbar-brand" to="/">Car Marketplace</Link>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/cars">Catalog</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/cars/create">Create Car</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/auth/login">Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/auth/register">Register</Link>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-nav d-flex flex-row gap-3 mb-0">
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/cars">Catalog</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/cars/create">Create</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/auth/login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/auth/register">Register</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   )
