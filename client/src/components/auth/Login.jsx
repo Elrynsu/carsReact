343ab6230f9c0ccf2dda7d87da1846e21e050dec
Teodor <elrynsier@gmail.com>
2025-04-01 14:33:42 +0300

    Login error handling with useRef inside the login func

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,18 +1,48 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { Link, useNavigate } from 'react-router';
 import { useLogin } from '../../api/authApi';
 import { UserContext } from '../../contexts/UserContext';
+import useErrorNotification from '../../hooks/useErrorNotification';
+import ErrorNotification from '../common/ErrorNotification';
 
 export default function Login() {
     const navigate = useNavigate();
     const { userLoginHandler } = useContext(UserContext);
     const { login } = useLogin();
+    const formRef = useRef(null);
+    const { error, showError, clearError } = useErrorNotification();
 
     const loginHandler = async (formData) => {
-        const values = Object.fromEntries(formData);
-        const authData = await login(values.email, values.password);
-        userLoginHandler(authData);
-        navigate('/cars');
+        clearError();
+
+        const form = formRef.current;
+
+        if (!form.checkValidity()) {
+            const firstInvalidInput = form.querySelector(':invalid');
+            const fieldName = firstInvalidInput.getAttribute('name');
+
+            const friendlyNames = {
+                email: 'Email',
+                password: 'Password',
+            };
+
+            const firstFieldName = friendlyNames[fieldName] || fieldName;
+            const message = `${firstFieldName}: ${firstInvalidInput.validationMessage}`;
+
+            showError(message);
+            setTimeout(() => clearError(), 3500);
+            return;
+        }  
+
+        try{
+            const values = Object.fromEntries(formData);
+            const authData = await login(values.email, values.password);
+            userLoginHandler(authData);
+            navigate('/cars');
+        }catch(err) {
+            showError(err.message || 'Login failed. Please try again.');
+            setTimeout(() => clearError(), 3500);
+        }
     };
 
     return (
@@ -23,15 +53,16 @@ export default function Login() {
                 </div>
 
                 <div className="card-body">
-                    <form action={loginHandler}>
+                    <ErrorNotification message={error} onClose={clearError} />
+                    <form ref={formRef} action={loginHandler} noValidate>
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email</label>
-                            <input type="email" className="form-control" id="email" name="email" placeholder="Your email..." required />
+                            <input type="email" className="form-control" id="email" name="email" placeholder="Your email..."  required />
                         </div>
 
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="password" name="password" placeholder="Your password..." required />
+                            <input type="password" className="form-control" id="password" name="password" placeholder="Your password..." minLength="5" maxLength="25" required />
                         </div>
 
                         <div className="d-grid">
